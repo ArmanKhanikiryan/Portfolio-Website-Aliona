13c8f5b2daa6d7fc29c004531794d6cd8d9b1990
Arman Khanikiryan <akhanikiryan@gmail.com>
2023-07-26 21:14:07 +0400

    animation link component

--- src/landing-page/links/Links.tsx
+++ src/landing-page/links/Links.tsx
@@ -8,19 +8,19 @@ import twitter from '@/assets/twitter-icon.png'
 const Links = () => {
     return (
         <div className='links_wrapper'>
-            <div className='links_line'></div>
+            <div data-aos="fade-up-right" data-aos-duration="1200" className='links_line'></div>
             <div className='links_components'>
                 <div className='links_underline'>
-                    <div className='links_components_info'>Website by</div>
-                    <div className='links_creator_name'>Arman Khanikiryan</div>
+                    <div data-aos="fade-up-right" data-aos-duration="1200" className='links_components_info'>Website by</div>
+                    <div data-aos="fade-up-right" data-aos-duration="1200" className='links_creator_name'>Arman Khanikiryan</div>
                 </div>
 
                 <div className='links_icons_wrapper'>
-                    <img id='last_lik_item' className='links_icons' src={instagram} alt="instagram"/>
-                    <img className='links_icons' src={facebook} alt="facebook"/>
-                    <img className='links_icons' src={linkedin} alt="linkedin"/>
-                    <img className='links_icons' src={twitter} alt="twitter"/>
-                    <img className='links_icons' src={youtube} alt="youtube"/>
+                    <img data-aos="fade-up-right" data-aos-duration="1200" id='last_lik_item' className='links_icons' src={instagram} alt="instagram"/>
+                    <img data-aos="fade-up-right" data-aos-duration="1200" className='links_icons' src={facebook} alt="facebook"/>
+                    <img data-aos="fade-up-right" data-aos-duration="1200" className='links_icons' src={linkedin} alt="linkedin"/>
+                    <img data-aos="fade-up-right" data-aos-duration="1200" className='links_icons' src={twitter} alt="twitter"/>
+                    <img data-aos="fade-up-right" data-aos-duration="1200" className='links_icons' src={youtube} alt="youtube"/>
                 </div>
             </div>
         </div>
