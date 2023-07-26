fe2936cf972be58b29768f20614ee6c1c3b2a163
Arman Khanikiryan <akhanikiryan@gmail.com>
2023-07-26 21:58:02 +0400

    redirects

--- src/landing-page/contact/Contact.tsx
+++ src/landing-page/contact/Contact.tsx
@@ -2,12 +2,15 @@ import './Contact.css'
 import whiteArrow from '@/assets/white-arrow.png'
 import arrow from '@/assets/black-arrow.png'
 const Contact = () => {
+    const handleRedirect = (url:string) => {
+        window.open(url, '_blank');
+    }
     return (
         <div className='contact_container'>
             <h1 data-aos="fade-up-right" data-aos-duration="1500" className='contact_title'>Contact</h1>
         <div className='contact_wrapper'>
             <div className='contact_filed_wrapper'>
-            <div className='contact_filed'>
+            <div onClick={() => handleRedirect('https://gmail.com')}  className='contact_filed'>
                 <div className='contact_filed_info'>
                     <div data-aos="fade-up-right" data-aos-duration="1500">Email me</div>
                     <img data-aos="fade-up-right" data-aos-duration="1500" className='arrow_icon' src={arrow} alt="arrow"/>
@@ -15,7 +18,7 @@ const Contact = () => {
                 </div>
                 <div data-aos="fade-up-right" data-aos-duration="1500" className='line'></div>
             </div>
-            <div className='contact_filed'>
+            <div onClick={() => handleRedirect('https://instagram.com')} className='contact_filed'>
                 <div className='contact_filed_info'>
                     <div data-aos="fade-up-right" data-aos-duration="1500">Instagram</div>
                     <img data-aos="fade-up-right" data-aos-duration="1500" className='arrow_icon' src={arrow} alt="arrow"/>
@@ -23,7 +26,7 @@ const Contact = () => {
                 </div>
                 <div data-aos="fade-up-right" data-aos-duration="1500" className='line'></div>
             </div>
-            <div className='contact_filed'>
+            <div onClick={() => handleRedirect('https://facebook.com')} className='contact_filed'>
                 <div className='contact_filed_info'>
                     <div data-aos="fade-up-right" data-aos-duration="1500">Facebook</div>
                     <img data-aos="fade-up-right" data-aos-duration="1500" className='arrow_icon' src={arrow} alt="arrow"/>
@@ -34,7 +37,7 @@ const Contact = () => {
             </div>
 
             <div className='contact_filed_wrapper'>
-                <div className='contact_filed'>
+                <div onClick={() => handleRedirect('https://twitter.com')} className='contact_filed'>
                     <div className='contact_filed_info'>
                         <div data-aos="fade-up-right" data-aos-duration="1500">Twitter</div>
                         <img data-aos="fade-up-right" data-aos-duration="1500" className='arrow_icon' src={arrow} alt="arrow"/>
@@ -42,7 +45,7 @@ const Contact = () => {
                     </div>
                     <div data-aos="fade-up-right" data-aos-duration="1500" className='line'></div>
                 </div>
-                <div className='contact_filed'>
+                <div onClick={() => handleRedirect('https://linkedin.com')} className='contact_filed'>
                     <div className='contact_filed_info'>
                         <div data-aos="fade-up-right" data-aos-duration="1500">Linkedin</div>
                         <img data-aos="fade-up-right" data-aos-duration="1500" className='arrow_icon' src={arrow} alt="arrow"/>
@@ -50,9 +53,9 @@ const Contact = () => {
                     </div>
                     <div data-aos="fade-up-right" data-aos-duration="1500" className='line'></div>
                 </div>
-                <div className='contact_filed'>
+                <div onClick={() => handleRedirect('https://youtube.com')} className='contact_filed'>
                     <div className='contact_filed_info'>
-                        <div data-aos="fade-up-right" data-aos-duration="1500">Dribble</div>
+                        <div data-aos="fade-up-right" data-aos-duration="1500">Youtube</div>
                         <img data-aos="fade-up-right" data-aos-duration="1500" className='arrow_icon' src={arrow} alt="arrow"/>
                         <img data-aos="fade-up-right" data-aos-duration="1500" className='arrow_icon_white' src={whiteArrow} alt="arrow"/>
                     </div>
